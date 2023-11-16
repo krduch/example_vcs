e0c5862ae8f9a76e3268596cd4099bbf65d34779
krduch <123241324+krduch@users.noreply.github.com>
2023-11-16 16:23:57 +0100

    drugi commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def dodawanie (x, y):
+    return x+y
+
+def odejmowanie (x, y):
+    return x - y
+
+print(f"zwróc nam wynik dodawania  {dodawanie(3,5)}")
+
+print(f"zwróc nam wynik odejmowania  {odejmowanie(3,5)}")
+
+
